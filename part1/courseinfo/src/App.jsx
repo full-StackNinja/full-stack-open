bf1c7d79fb3e86bb49f329834d92ebc620794df6
Imran Hussain <fswebenthusiast@gmail.com>
2024-04-25 07:48:46 +0500

    Modify content structure to object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,55 +4,34 @@ import Total from "./Components/Total";
 
 const App = () => {
    const course = "Half Stack application development";
-   const part1 = "Fundamentals of React";
-   const exercises1 = 10;
-   const part2 = "Using props to pass data";
-   const exercises2 = 7;
-   const part3 = "State of a component";
-   const exercises3 = 14;
-   const content = {
-      part1,
-      exercises1,
-      part2,
-      exercises2,
-      part3,
-      exercises3,
+   const part1 = {
+      name: "Fundamentals of React",
+      exercises: 10,
    };
+   const part2 = {
+      name: "Using props to pass data",
+      exercises: 7,
+   };
+   const part3 = {
+      name: "State of a component",
+      exercises: 14,
+   };
+
    return (
       <>
          <Header course={course} />
-         <Content {...content} />
-         <Total total={{ exercises1, exercises2, exercises3 }} />
+         <Content
+            part1={part1}
+            part2={part2}
+            part3={part3}
+         />
+         <Total
+            exercises1={part1["exercises"]}
+            exercises2={part2["exercises"]}
+            exercises3={part3["exercises"]}
+         />
       </>
    );
 };
 
 export default App;
-
-// const App = () => {
-//    const course = 'Half Stack application development'
-//    const part1 = 'Fundamentals of React'
-//    const exercises1 = 10
-//    const part2 = 'Using props to pass data'
-//    const exercises2 = 7
-//    const part3 = 'State of a component'
-//    const exercises3 = 14
-
-//    return (
-//      <div>
-//        <h1>{course}</h1>
-//        <p>
-//          {part1} {exercises1}
-//        </p>
-//        <p>
-//          {part2} {exercises2}
-//        </p>
-//        <p>
-//          {part3} {exercises3}
-//        </p>
-//        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-//      </div>
-//    )
-//  }
-
-//  export default App
