d79ef9399f001d32bd1b201e77312788cd7156e2
Imran Hussain <fswebenthusiast@gmail.com>
2024-04-27 10:55:06 +0500

    refactor: change code to render Stats component conditionally

--- part1/unicafe/src/Stats.jsx
+++ part1/unicafe/src/Stats.jsx
@@ -1,14 +1,20 @@
 const Content = ({ stats }) => {
    return (
-      <div>
+      <>
          <h2>Statistics</h2>
-         <p>good: {stats.good} </p>
-         <p>neutral: {stats.neutral}</p>
-         <p>bad: {stats.bad} </p>
-         <p>all: {stats.all}</p>
-         <p>average: {stats.average}</p>
-         <p>positive feedback: {stats.positiveFeedback}%</p>
-      </div>
+         {stats.all ? (
+            <div>
+               <p>good: {stats.good} </p>
+               <p>neutral: {stats.neutral}</p>
+               <p>bad: {stats.bad} </p>
+               <p>all: {stats.all}</p>
+               <p>average: {stats.average}</p>
+               <p>positive feedback: {stats.positiveFeedback}%</p>
+            </div>
+         ) : (
+            <p>No feedback given</p>
+         )}
+      </>
    );
 };
 
