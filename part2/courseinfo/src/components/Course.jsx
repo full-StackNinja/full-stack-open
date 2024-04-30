86343ca93f49bf2707eb727a4a65885ef98d76a3
Imran Hussain <fswebenthusiast@gmail.com>
2024-04-30 19:57:19 +0500

    refactor: modify return statement in App component to render multiple number of courses by using single Course component

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,13 +1,13 @@
 const Header = ({ name }) => <h1>{name}</h1>;
 const Content = ({ parts }) => {
    return (
-      <>
+      <ul>
          {parts.map((part) => (
             <li key={part.id}>
                {part.name} {part.exercises}
             </li>
          ))}
-      </>
+      </ul>
    );
 };
 
