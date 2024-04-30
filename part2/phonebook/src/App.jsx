1890adf24abbb94b7eb66f47a9a341f91dc9c581
Imran Hussain <fswebenthusiast@gmail.com>
2024-04-30 22:19:46 +0500

    Add phone number field

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+   const [persons, setPersons] = useState([
+      { name: "Arto Hellas", phone: "0332-876598" },
+   ]);
+   const [newName, setNewName] = useState("");
+   const [newPhone, setNewPhone] = useState("");
 
-  return (
-    <>
+   const addPerson = (e) => {
+      e.preventDefault();
+      const personList = [...persons];
+      const person = personList.filter((person) => person.name === newName);
+      if (person.length)
+         return alert(`${person[0].name} is already added to the phonebook!}`);
+      personList.push({ name: newName, phone: newPhone });
+      setPersons(personList);
+      setNewName("");
+      setNewPhone("");
+   };
+
+   return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+         <h2>Phonebook</h2>
+         <form onSubmit={addPerson}>
+            <div>
+               name:{" "}
+               <input
+                  value={newName}
+                  onChange={(e) => setNewName(e.target.value)}
+               />
+            </div>
+            <div>
+               phone:{" "}
+               <input
+                  value={newPhone}
+                  onChange={(e) => setNewPhone(e.target.value)}
+               />
+            </div>
+            <div>
+               <button type="submit">add</button>
+            </div>
+         </form>
+         <h2>Numbers</h2>
+         <ul>
+            {persons.map((person, index) => (
+               <li key={index}>
+                  {person.name} {person.phone}
+               </li>
+            ))}
+         </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+   );
+};
 
-export default App
+export default App;
