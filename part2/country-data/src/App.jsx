57b4b0e4538d1049859bcef3334aa1324524bdf1
Imran Hussain <fswebenthusiast@gmail.com>
2024-05-02 19:59:34 +0500

    Initial commit

---
+++ part2/country-data/src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+const App = () => {
+   const [countries, setCountries] = useState(null);
+
+   useEffect(()=>{
+    
+   }, [])
+
+   return <></>;
+};
+
+export default App;
