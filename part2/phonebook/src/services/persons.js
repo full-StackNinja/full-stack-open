9327a2233f03e8378c8c9942c7ec579d8945d9e8
Imran Hussain <fswebenthusiast@gmail.com>
2024-05-12 18:06:27 +0500

    fix: change baseUrl string to variable

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -8,14 +8,14 @@ const addPerson = (newPerson) => axios.post(baseUrl, newPerson);
 
 const deletePerson = (person) => {
    if (window.confirm(`Do you want to delete ${person.name}?`))
-      return axios.delete(`baseUrl/${person.id}`);
+      return axios.delete(`${baseUrl}/${person.id}`);
    return new Promise((resolve, reject) => {
       return reject("user aborted the delete operation");
    });
 };
 
 const updatePerson = (id, newPerson) => {
-   return axios.put(`baseUrl/${id}`, newPerson);
+   return axios.put(`${baseUrl}/${id}`, newPerson);
 };
 
 export default {
